0575af900bb6d1e8c633537f119349d69da0e974
teona piranishvili <teona.piranishvili1@gmail.com>
2022-11-01 19:41:02 +0400

    Fixing some bugs

--- index.js
+++ index.js
@@ -8,60 +8,57 @@ const imgThree = document.getElementById("3");
 const btnLeft = document.getElementById("left-btn");
 const btnRight = document.getElementById("right-btn");
 
-let num = 2;
-let left = 3;
-let right = 4;
+let num = 1;
+let left = 2;
+let right = 3;
 
+btnLeft.addEventListener("click", () => {
+  leftFunction();
+});
 btnRight.addEventListener("click", () => {
-  if (num == 0 || left == 0 || right == 0) {
-    imgOne.src = `./assets/image-slide-1.jpg`;
-    imgTwo.src = `./assets/image-slide-2.jpg`;
-    imgThree.src = `./assets/image-slide-3.jpg`;
+  rightFunction();
+});
+
+const rightFunction = () => {
+  num++;
+  left++;
+  right++;
+  if (right == 6) {
+    right = 1;
+  }
+  if (left == 6) {
+    right = 2;
+    left = 1;
+  }
+  if (num == 6) {
+    right = 2;
+    left = 1;
+    num = 5;
+  }
+  changeImg(num, left, right);
+};
+
+const leftFunction = () => {
+  num--;
+  left--;
+  right--;
+  if (right == 0) {
+    right = 5;
+  }
+  if (left == 0) {
+    right = 5;
+    left = 5;
+  }
+  if (num == 0) {
     num = 1;
     left = 2;
     right = 3;
   }
-  if (num <= 5) {
-    imgOne.src = `./assets/image-slide-${num}.jpg`;
-    imgTwo.src = `./assets/image-slide-${left}.jpg`;
-    if (right < 6) {
-      imgThree.src = `./assets/image-slide-${right}.jpg`;
-    } else {
-      imgThree.src = `./assets/image-slide-2.jpg`;
-    }
-    if (left < 6) {
-      imgTwo.src = `./assets/image-slide-${left}.jpg`;
-    } else {
-      imgTwo.src = `./assets/image-slide-1.jpg`;
-    }
-  }
-  num = num + 1;
-  left = left + 1;
-  right = right + 1;
-});
+  changeImg(num, left, right);
+};
 
-btnLeft.addEventListener("click", () => {
-  if (num == 0 || left == 0 || right == 0) {
-    imgOne.src = `./assets/image-slide-1.jpg`;
-    imgTwo.src = `./assets/image-slide-2.jpg`;
-    imgThree.src = `./assets/image-slide-3.jpg`;
-    num = num + 1;
-    left = left + 1;
-    right = right + 1;
-  } else if (num >= 5 || left >= 5 || right >= 5) {
-    imgOne.src = `./assets/image-slide-4.jpg`;
-    imgTwo.src = `./assets/image-slide-5.jpg`;
-    imgThree.src = `./assets/image-slide-1.jpg`;
-    num = 4;
-    left = 5;
-    right = 3;
-  } else {
-    imgOne.src = `./assets/image-slide-${num}.jpg`;
-    imgTwo.src = `./assets/image-slide-${left}.jpg`;
-    imgThree.src = `./assets/image-slide-${right}.jpg`;
-  }
-
-  num = num - 1;
-  left = left - 1;
-  right = right - 1;
-});
+const changeImg = (a, b, c) => {
+  imgOne.src = `./assets/image-slide-${a}.jpg`;
+  imgTwo.src = `./assets/image-slide-${b}.jpg`;
+  imgThree.src = `./assets/image-slide-${c}.jpg`;
+};
